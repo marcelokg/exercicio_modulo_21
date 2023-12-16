d19b63b9e97322de9c0b1d57c0a272d823412729
Marcelo Kriiger <marcelokriiger@outlook.com>
2023-12-16 16:42:29 -0300

    finalização

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('.header')
 
-    header.classList.toggle('header__scrolling', window.scrollY > 50)
+document.addEventListener('DOMContentLoaded', function(){
+    const questions = document.querySelectorAll('[data-faq-questions]')
+    
+    
+    window.addEventListener("scroll", function(){
+        let header = document.querySelector('.header')
+
+        header.classList.toggle('header__scrolling', window.scrollY > 50)
+    })
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is--open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
+
+
